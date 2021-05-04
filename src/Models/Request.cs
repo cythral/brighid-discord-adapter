49ca4ce44c61be535b7145aa411e67178a771491
Talen Fisher <talen.fisher@cythral.com>
2021-05-04 12:59:26 -0500

    BRGD-61 Nullability + Request Constructor

--- src/Models/Request.cs
+++ src/Models/Request.cs
@@ -10,6 +10,20 @@ namespace Brighid.Discord.Models
     public struct Request
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="Request" /> struct.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to use for the request.</param>
+        public Request(Endpoint endpoint)
+        {
+            Id = Guid.NewGuid();
+            Endpoint = endpoint;
+            RequestBody = null;
+            ResponseQueueURL = null;
+            Parameters = new Dictionary<string, string>();
+            Headers = new Dictionary<string, string>();
+        }
+
+        /// <summary>
         /// Gets or sets the request ID.
         /// </summary>
         [JsonPropertyName("id")]
@@ -25,13 +39,13 @@ namespace Brighid.Discord.Models
         /// Gets or sets the request body.
         /// </summary>
         [JsonPropertyName("b")]
-        public string RequestBody { get; set; }
+        public string? RequestBody { get; set; }
 
         /// <summary>
         /// Gets or sets the response queue URL.
         /// </summary>
         [JsonPropertyName("r")]
-        public Uri ResponseQueueURL { get; set; }
+        public Uri? ResponseQueueURL { get; set; }
 
         /// <summary>
         /// Gets or sets the request parameters to be used in the endpoint's path template.
