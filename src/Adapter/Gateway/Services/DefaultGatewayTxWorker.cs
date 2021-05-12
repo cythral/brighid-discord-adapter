f0e96c705c8fae256de197120f55ba3fa0012827
Talen Fisher <talen.fisher@cythral.com>
2021-05-12 09:58:13 -0500

    BRGD-74 Gateway TX Worker Refactor - Use Timer

--- src/Adapter/Gateway/Services/DefaultGatewayTxWorker.cs
+++ src/Adapter/Gateway/Services/DefaultGatewayTxWorker.cs
@@ -17,78 +17,96 @@ namespace Brighid.Discord.Adapter.Gateway
         private const string WorkerThreadName = "Gateway TX";
         private readonly IChannel<GatewayMessage> channel;
         private readonly ISerializer serializer;
+        private readonly ITimerFactory timerFactory;
         private readonly IGatewayUtilsFactory gatewayUtilsFactory;
         private readonly ILogger<DefaultGatewayTxWorker> logger;
-        private CancellationToken cancellationToken;
-        private IWorkerThread? workerThread;
+        private ITimer? timer;
         private IClientWebSocket? webSocket;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DefaultGatewayTxWorker" /> class.
         /// </summary>
         /// <param name="serializer">Serializer used for deserializing messages from the gateway.</param>
+        /// <param name="timerFactory">Factory used to create timers.</param>
         /// <param name="gatewayUtilsFactory">Factory to create various utils with.</param>
         /// <param name="logger">Logger used to log information to some destination(s).</param>
         public DefaultGatewayTxWorker(
             ISerializer serializer,
+            ITimerFactory timerFactory,
             IGatewayUtilsFactory gatewayUtilsFactory,
             ILogger<DefaultGatewayTxWorker> logger
         )
         {
             this.serializer = serializer;
+            this.timerFactory = timerFactory;
             this.logger = logger;
             this.gatewayUtilsFactory = gatewayUtilsFactory;
             channel = gatewayUtilsFactory.CreateChannel<GatewayMessage>();
         }
 
         /// <inheritdoc />
-        public void Start(IGatewayService gateway, IClientWebSocket webSocket, CancellationTokenSource cancellationTokenSource)
+        public bool IsRunning { get; private set; }
+
+        /// <inheritdoc />
+        public async Task Start(IGatewayService gateway, IClientWebSocket webSocket)
         {
-            cancellationToken = cancellationTokenSource.Token;
+            IsRunning = true;
             this.webSocket = webSocket;
-            workerThread = gatewayUtilsFactory.CreateWorkerThread(Run, WorkerThreadName);
-            workerThread.OnUnexpectedStop = () => gateway.Restart();
-            workerThread.Start(cancellationTokenSource);
+
+            timer = timerFactory.CreateTimer(Run, 0, WorkerThreadName);
+            timer.StopOnException = true;
+            timer.OnUnexpectedStop = () => gateway.Restart();
+            await timer.Start();
         }
 
         /// <inheritdoc />
-        public void Stop()
+        public async Task Stop()
         {
-            workerThread?.Stop();
-            workerThread = null;
+            IsRunning = false;
+            await timer!.Stop();
+            timer = null;
         }
 
         /// <inheritdoc />
         public async Task Emit(GatewayMessage message, CancellationToken cancellationToken)
         {
-            this.cancellationToken.ThrowIfCancellationRequested();
             cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfNotRunning();
             await channel.Write(message, cancellationToken);
         }
 
         /// <summary>
         /// Runs the Gateway TX Worker.
         /// </summary>
+        /// <param name="cancellationToken">Token used to cancel the operation.</param>
         /// <returns>The resulting task.</returns>
-        public async Task Run()
+        public async Task Run(CancellationToken cancellationToken = default)
         {
-            while (!cancellationToken.IsCancellationRequested)
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfNotRunning();
+
+            while (webSocket?.State != WebSocketState.Open)
             {
-                while (webSocket?.State != WebSocketState.Open)
-                {
-                    await gatewayUtilsFactory.CreateDelay(100, cancellationToken);
-                }
+                await gatewayUtilsFactory.CreateDelay(100, cancellationToken);
+            }
 
-                if (!await channel.WaitToRead(cancellationToken))
-                {
-                    continue;
-                }
+            if (!await channel.WaitToRead(cancellationToken))
+            {
+                return;
+            }
 
-                var message = await channel.Read(cancellationToken);
-                var serializedMessage = await serializer.SerializeToBytes(message, cancellationToken);
+            var message = await channel.Read(cancellationToken);
+            var serializedMessage = await serializer.SerializeToBytes(message, cancellationToken);
 
-                logger.LogInformation("Sending Message: {@message}", message);
-                await webSocket!.Send(serializedMessage, WebSocketMessageType.Text, true, cancellationToken);
+            logger.LogInformation("Sending Message: {@message}", message);
+            await webSocket!.Send(serializedMessage, WebSocketMessageType.Text, true, cancellationToken);
+        }
+
+        private void ThrowIfNotRunning()
+        {
+            if (!IsRunning)
+            {
+                throw new System.OperationCanceledException();
             }
         }
     }
