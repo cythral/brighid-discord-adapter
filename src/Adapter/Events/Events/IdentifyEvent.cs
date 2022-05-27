d03c9d4e82e76aa1b125dae8dfaca992c1d0fa26
Talen Fisher <talen.fisher@cythral.com>
2022-05-26 23:19:55 -0500

    Revert changes to IdentifyEvent

--- src/Adapter/Events/Events/IdentifyEvent.cs
+++ src/Adapter/Events/Events/IdentifyEvent.cs
@@ -10,49 +10,49 @@ namespace Brighid.Discord.Adapter.Events
     /// Used to trigger the initial handshake with the gateway.
     /// </summary>
     [GatewayEvent(GatewayOpCode.Identify)]
-    public readonly struct IdentifyEvent : IGatewayEvent
+    public struct IdentifyEvent : IGatewayEvent
     {
         /// <summary>
-        /// Gets the authentication token.
+        /// Gets or sets the authentication token.
         /// </summary>
         [JsonPropertyName("token")]
         [NotLogged]
-        public string Token { get; init; }
+        public string Token { get; set; }
 
         /// <summary>
-        /// Gets the connection properties.
+        /// Gets or sets the connection properties.
         /// </summary>
         [JsonPropertyName("properties")]
-        public ConnectionProperties ConnectionProperties { get; init; }
+        public ConnectionProperties ConnectionProperties { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether this connection supports compression of packets.
+        /// Gets or sets a value indicating whether this connection supports compression of packets.
         /// </summary>
         [JsonPropertyName("compress")]
-        public bool IsCompressionSupported { get; init; }
+        public bool IsCompressionSupported { get; set; }
 
         /// <summary>
-        /// Gets a value between 50 and 250, total number of members where the gateway will stop sending offline members in the guild member list.
+        /// Gets or sets a value between 50 and 250, total number of members where the gateway will stop sending offline members in the guild member list.
         /// </summary>
         [JsonPropertyName("large_threshold")]
-        public int LargeGuildThreshold { get; init; }
+        public int LargeGuildThreshold { get; set; }
 
         /// <summary>
-        /// Gets array of two integers (shard_id, num_shards) used for Guild Sharding.
+        /// Gets or sets array of two integers (shard_id, num_shards) used for Guild Sharding.
         /// </summary>
         [JsonPropertyName("shard")]
-        public int[] Shard { get; init; }
+        public int[] Shard { get; set; }
 
         /// <summary>
-        /// Gets presence structure for initial presence information.
+        /// Gets or sets presence structure for initial presence information.
         /// </summary>
         [JsonPropertyName("presence")]
-        public PresenceUpdate? Presence { get; init; }
+        public PresenceUpdate? Presence { get; set; }
 
         /// <summary>
-        /// Gets the Gateway Intents you wish to receive.
+        /// Gets or sets the Gateway Intents you wish to receive.
         /// </summary>
         [JsonPropertyName("intents")]
-        public Intent Intents { get; init; }
+        public Intent Intents { get; set; }
     }
 }
