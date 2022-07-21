92289903b94fb0b1646eb3a0c00c74e92c7623f9
Talen Fisher <talen.fisher@cythral.com>
2022-07-20 23:24:55 -0500

    use index 1

--- .github/scripts/reject-old-deployments.sh
+++ .github/scripts/reject-old-deployments.sh
@@ -5,7 +5,7 @@ repo=$1
 escaped_repo=$(echo $repo | xargs | sed 's/\//\\\//')
 
 environment_id=$(gh api repos/$repo/environments --jq '[.environments[] | select(.name == "Production")][0].id')
-deployment_id=$(gh api repos/$repo/deployments --jq '[.[] | select(.environment == "Production")][0].id')
+deployment_id=$(gh api repos/$repo/deployments --jq '[.[] | select(.environment == "Production")][1].id')
 target_url=$(gh api repos/$repo/deployments/$deployment_id/statuses --jq '.[0].target_url')
 job_id=$(echo $target_url | sed -E "s/https:\/\/github.com\/${escaped_repo}\/runs\/([0-9]+).*/\1/")
 run_id=$(gh api repos/$repo/actions/jobs/$job_id --jq '.run_id')
