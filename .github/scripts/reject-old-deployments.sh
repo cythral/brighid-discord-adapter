496841027e62593ed851650091bec41d6aed2f82
Talen Fisher <talen.fisher@cythral.com>
2022-07-20 21:23:52 -0500

    fix has_pending_deployment logic

--- .github/scripts/reject-old-deployments.sh
+++ .github/scripts/reject-old-deployments.sh
@@ -5,17 +5,26 @@ repo=$1
 escaped_repo=$(echo $repo | xargs | sed 's/\//\\\//')
 
 environment_id=$(gh api repos/$repo/environments --jq '[.environments[] | select(.name == "Production")][0].id')
-deployment_id=$(gh api repos/$repo/deployments --jq '[.[] | select(.environment == "Production")][1].id')
+deployment_id=$(gh api repos/$repo/deployments --jq '[.[] | select(.environment == "Production")][0].id')
 target_url=$(gh api repos/$repo/deployments/$deployment_id/statuses --jq '.[0].target_url')
 job_id=$(echo $target_url | sed -E "s/https:\/\/github.com\/${escaped_repo}\/runs\/([0-9]+).*/\1/")
 run_id=$(gh api repos/$repo/actions/jobs/$job_id --jq '.run_id')
 
 function has_pending_deployment() {
-    gh api repos/$repo/actions/runs/$run_id/pending_deployments || return 0
-    return 1
+    set +eo pipefail
+    count=$(gh api repos/$repo/actions/runs/$run_id/pending_deployments --jq ". | length")
+    local result=$?
+    set -eo pipefail
+    
+    if [ "$result" = "0" ] && [ "$count" != "0" ]; then
+        echo "true"
+        return
+    fi
+
+    echo "false"
 }
 
-if has_pending_deployment; then
+if [ "$(has_pending_deployment)" = "true" ]; then
     gh api \
         --method POST \
         repos/$repo/actions/runs/$run_id/pending_deployments \
