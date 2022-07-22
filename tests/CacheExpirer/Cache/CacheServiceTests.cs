f66e39793c913a9cb8bb1f417d134aefa3f86e9f
Talen Fisher <talen.fisher@cythral.com>
2022-07-22 08:41:11 -0500

    Request ipv6 addresses

--- tests/CacheExpirer/Cache/CacheServiceTests.cs
+++ tests/CacheExpirer/Cache/CacheServiceTests.cs
@@ -22,15 +22,16 @@ namespace Brighid.Discord.CacheExpirer
         {
             [Test, Auto]
             public async Task ShouldSendADeleteRequestToTheClearUserCacheEndpoint(
-                IPAddress ip,
                 string userId,
                 [Frozen] MockHttpMessageHandler handler,
                 [Target] CacheService cache,
                 CancellationToken cancellationToken
             )
             {
+                var ip = IPAddress.Parse("9d89:15fe:df6f:6634:d1ed:6ae3:ef11:d0cb");
+
                 handler
-                .Expect(HttpMethod.Delete, $"http://{ip}/cache/users/{userId}")
+                .Expect(HttpMethod.Delete, $"http://[{ip}]/cache/users/{userId}")
                 .Respond(HttpStatusCode.OK);
 
                 await cache.ExpireUserCache(ip, userId, cancellationToken);
@@ -40,15 +41,16 @@ namespace Brighid.Discord.CacheExpirer
 
             [Test, Auto]
             public async Task ShouldThrowIfNonSuccessfulStatusCode(
-                IPAddress ip,
                 string userId,
                 [Frozen] MockHttpMessageHandler handler,
                 [Target] CacheService cache,
                 CancellationToken cancellationToken
             )
             {
+                var ip = IPAddress.Parse("b6e7:3d3b:b71c:0594:5012:49f6:e6d3:6270");
+
                 handler
-                .Expect(HttpMethod.Delete, $"http://{ip}/cache/users/{userId}")
+                .Expect(HttpMethod.Delete, $"http://[{ip}]/cache/users/{userId}")
                 .Respond(HttpStatusCode.InternalServerError);
 
                 Func<Task> func = () => cache.ExpireUserCache(ip, userId, cancellationToken);
@@ -64,15 +66,16 @@ namespace Brighid.Discord.CacheExpirer
         {
             [Test, Auto]
             public async Task ShouldSendADeleteRequestToTheClearCommandCacheEndpoint(
-                IPAddress ip,
                 string userId,
                 [Frozen] MockHttpMessageHandler handler,
                 [Target] CacheService cache,
                 CancellationToken cancellationToken
             )
             {
+                var ip = IPAddress.Parse("b47e:925f:4be9:0c72:4a14:1290:0270:5a9b");
+
                 handler
-                .Expect(HttpMethod.Delete, $"http://{ip}/cache/commands/{userId}")
+                .Expect(HttpMethod.Delete, $"http://[{ip}]/cache/commands/{userId}")
                 .Respond(HttpStatusCode.OK);
 
                 await cache.ExpireCommandCache(ip, userId, cancellationToken);
@@ -82,15 +85,16 @@ namespace Brighid.Discord.CacheExpirer
 
             [Test, Auto]
             public async Task ShouldThrowIfNonSuccessfulStatusCode(
-                IPAddress ip,
                 string userId,
                 [Frozen] MockHttpMessageHandler handler,
                 [Target] CacheService cache,
                 CancellationToken cancellationToken
             )
             {
+                var ip = IPAddress.Parse("c034:05ed:8e31:d24a:af63:b2bf:7d2b:7288");
+
                 handler
-                .Expect(HttpMethod.Delete, $"http://{ip}/cache/commands/{userId}")
+                .Expect(HttpMethod.Delete, $"http://[{ip}]/cache/commands/{userId}")
                 .Respond(HttpStatusCode.InternalServerError);
 
                 Func<Task> func = () => cache.ExpireCommandCache(ip, userId, cancellationToken);
